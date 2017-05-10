c8fc53f4ddee59b0715b362c6ebbd73834383f59
Karl Wallenius <karl.wallenius@gmail.com>
2017-05-10 17:13:59 +0200

    Fixed function description and help

--- PSScaffold/Public/New-PSPesterTest.ps1
+++ PSScaffold/Public/New-PSPesterTest.ps1
@@ -3,20 +3,20 @@
     Creates a new Pester Test file.
 .DESCRIPTION
     Function to scaffold the structure of a test file for Pester tests.
-.PARAMETER Module
-    <String> The name of a module, or the parent path of a module manifest.
 .PARAMETER Name
     <String> The name of the function to create a test for.
+.PARAMETER Module
+    <String> The name of a module, or the parent path of a module manifest.
 .PARAMETER Scope
     <String> The scope of the function according to Redeploy scaffolding standards. Allowed values: Private and Public. Default: Public.
 .EXAMPLE
     To create a new Pester test for the function New-Action in Module 'MyModule', by Module name, and place it in the public scope.
 
-    New-REPesterTest -Module MyModule -Name New-Action
+    New-REPesterTest -Name New-Action -Module MyModule 
 .EXAMPLE
     To create a new Pester test for the function New-Action in Module 'MyModule', by Module path, and place it in the public scope.
 
-    New-PSPesterTest -Module C:\Users\JohnSmith\Documents\Projects\Modules\MyModule -Name New-Action
+    New-PSPesterTest -Name New-Action -Module C:\Users\JohnSmith\Documents\Projects\Modules\MyModule 
 .NOTES
     Written by Karl Wallenius, Redeploy AB.
 #>
