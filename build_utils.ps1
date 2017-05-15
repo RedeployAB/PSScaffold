f88e34959fc00a7d49b1d2c5cfa06e59d403650a
Karl Wallenius <karl.wallenius@gmail.com>
2017-05-15 12:39:14 +0200

    Changed build function

--- build_utils.ps1
+++ build_utils.ps1
@@ -1,16 +1,22 @@
 ﻿<#
 .SYNOPSIS
-    Published a PowerShell Module to a network share.
+    Publishe a PowerShell to a PSRepository.
+.DESCRIPTION
+    This function publishes a module to a PSRepository. It checks the current version on the destination
+    repository and compares it with the build number. It updates the Module Manifest at publishing.
 .NOTES
     Written by Michael Willis xainey@github - https://github.com/xainey.
+    Reworked by Karl Wallenius, KarlGW@github - https://github.com/KarlGW
+
+    Supports PSGallery as well after update.
 #>
-function Publish-SMBModule {
+function Publish-PSModule {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
         [string]
         $RepositoryName,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]
         $RepositoryPath,
         [Parameter(Mandatory=$true)]
@@ -19,10 +25,14 @@ function Publish-SMBModule {
         [Parameter(Mandatory=$true)]
         [string]
         $ModulePath,
+        [Parameter(Mandatory=$false)]
+        $ApiKey,
         [Parameter(Mandatory=$true)]
         [int]
         $BuildNumber
     )
+
+    $psGalleryName = 'PSGallery'
 
     # Test if the repository is registered.
     Write-Verbose ("Checking if Repository: {0} is registered." -f $RepositoryName)
@@ -32,12 +42,14 @@ function Publish-SMBModule {
             throw "The path does not exist. Please connect to the share."
         } else {
             # Register the Repository.
-            Write-Verbose("Registering Repository: {0}." -f $RepositoryName)
-            Register-PSRepository 
-                -Name $RepositoryName 
-                -SourceLocation $RepositoryPath 
-                -PublishLocation $RepositoryPath 
-                -InstallationPolicy Trusted
+            if ($RepositoryName -ne $psGalleryName) {
+                Write-Verbose("Registering Repository: {0}." -f $RepositoryName)
+                Register-PSRepository 
+                    -Name $RepositoryName 
+                    -SourceLocation $RepositoryPath 
+                    -PublishLocation $RepositoryPath 
+                    -InstallationPolicy Trusted
+            }
         }
     }
 
@@ -53,9 +65,23 @@ function Publish-SMBModule {
     # Publish Module.
     Write-Verbose ("Publishing Module: {0}." -f $ModuleName)
 
+    $publishParams = @{
+        Path = ".\$ModuleName"
+    }
+    # Determine type of publish.
+    if ($RepositoryName -eq $psGalleryName) {
+        if ([string]::IsNullOrEmpty($ApiKey)) {
+            throw("Please pass on a NuGet API key to deploy to the PSGallery.")
+        } else {
+            $publishParams.NuGetApiKey = $ApiKey
+        }
+    } else {
+        $publishParams.Repository = $RepositoryName
+    }
+
     try {
 
-        Publish-Module -Repository $RepositoryName -Path ".\$ModuleName"
+        Publish-Module @publishParams
 
     } catch [System.Exception] {
         throw($_.Exception)
