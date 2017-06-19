b705bbcaf8a8696cb4a94edccf8a1f76bcd75170
Karl Wallenius <karl.wallenius@gmail.com>
2017-06-19 17:35:11 +0200

    Added tests for Publish-PSModule

---
+++ Tests/Public/Publish-PSModule.Tests.ps1
@@ -0,0 +1,180 @@
+﻿$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("Tests\Public","PSScaffold\Public")
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+
+. "$here\$sut"
+
+Import-Module (Resolve-Path .\PSScaffold\PSScaffold.psm1) -Force -NoClobber
+
+InModuleScope "PSScaffold" {
+
+    Describe "Publish-PSModule" {
+
+        Mock Register-PSRepository { } # void
+
+        $testPath = "$env:TEMP\TestModules"
+        New-Item -ItemType Directory -Path $testPath
+
+        New-PSModule -Name TestModule -Path "$testPath" -Author 'Test' -Description 'Test'
+
+        It "Should publish the module to a repository." {
+
+            Mock Get-PSRepository { return @{
+                Name = $RepositoryName
+            }}
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'TestRepo'
+                RepositoryPath = '\\path\to\repo'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should Not throw
+        }
+
+        It "Should publish te module to a repository and update it's module manifest." {
+
+            Mock Get-PSRepository { return @{
+                Name = $RepositoryName
+            }}
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $true }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'TestRepo'
+                RepositoryPath = '\\path\to\repo'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should Not throw
+
+            $moduleVersion = (Get-Content "$testPath\TestModule\TestModule\TestModule.psd1" | select-string ".*ModuleVersion.*") -replace "\s" -replace "\w*=" -replace "'"
+
+            $moduleVersion | Should Be "1.0.1"
+
+        }
+
+        It "Should publish the module to PSGallery with an API key." {
+            Mock Get-PSRepository { return @{
+                Name = $RepositoryName
+            }}
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'PSGallery'
+                ApiKey = "ABCDEFGH"
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should Not throw
+        }
+
+        It "Should throw the module if PSGallery is specified without an API key." {
+            Mock Get-PSRepository { return @{
+                Name = $RepositoryName
+            }}
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'PSGallery'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should throw
+        }
+
+        It "Should throw if it cannot find the target Repository Path if Repository is not registered" {
+            Mock Get-PSRepository { return $false }
+
+            Mock Test-Path { return $false }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'TestRepo'
+                RepositoryPath = '\\path\to\repo'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should throw
+        }
+
+        It "Should register the repository if it's not registered and not 'PSGallery' is specified." {
+            Mock Get-PSRepository { return $false }
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { }
+
+            $publishParams = @{
+                RepositoryName = 'TestRepo'
+                RepositoryPath = '\\path\to\repo'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should not throw
+        }
+
+        It "Should throw if 'Publish-Module' fails" {
+            Mock Get-PSRepository { return @{
+                Name = $RepositoryName
+            }}
+
+            Mock Test-Path { return $true }
+
+            Mock Find-Module { return $false }
+
+            Mock Publish-Module { throw }
+
+            Mock Register-PSRepository { }
+
+            $publishParams = @{
+                RepositoryName = 'TestRepo'
+                RepositoryPath = '\\path\to\repo'
+                ModuleName = 'TestModule'
+                ModulePath = "$testPath\TestModule\TestModule\TestModule.psd1"
+                BuildNumber = 1
+            }
+
+            { Publish-PSModule @publishParams } | Should throw
+        }
+
+        Remove-Item -Path $testPath -Recurse -Force
+    }
+
+}
