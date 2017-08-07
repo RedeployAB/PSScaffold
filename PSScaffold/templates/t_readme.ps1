7ef0570daef4844d552f816f67762fbe3bcb2971
Karl Wallenius <karl.wallenius@gmail.com>
2017-08-07 11:23:44 +0200

    Added readme template

---
+++ PSScaffold/templates/t_readme.ps1
@@ -0,0 +1,30 @@
+$ReadmeContent = @"
+# <module>
+
+**Content**
+
+* [Introduction](#intro)
+* [Installation](#install)
+* [Functions](#functions)
+* [Usage Examples](#usage)
+* [Versions and Updates](#version)
+
+## <a name=intro>Introduction</a>
+
+
+## <a name=install>Installation</a>
+
+
+## <a name="functions">Functions</a>
+
+### Public functions
+
+
+### Private functions
+
+
+## <a name=usage>Usage Examples</a>
+
+
+## <a name=version>Versions and Updates</a>
+"@
