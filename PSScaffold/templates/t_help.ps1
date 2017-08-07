3f0f17e231a817fcb3a00eb4095f49e74da93031
Karl Wallenius <karl.wallenius@gmail.com>
2017-08-07 11:34:52 +0200

    Added some spacing in template

--- PSScaffold/templates/t_help.ps1
+++ PSScaffold/templates/t_help.ps1
@@ -1,10 +1,13 @@
 $HelpFileContent = @"
 PSTOPIC
     about_<module>
+
 SHORT DESCRIPTION
     A short description.
+
 LONG DESCRIPTION
     A longer description.
+
 DETAILED DESCRIPTION
     A longer more detailed description.
 "@
